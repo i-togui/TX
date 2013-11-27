b4173999c4cafe2bba3f4fb614687036b65e6624
i-togui <ilyass.togui@utt.fr>
2013-11-27 13:37:01 +0100

    New stuff

---
+++ MMProject/src/core/Tools.java
@@ -0,0 +1,19 @@
+package core;
+
+import android.util.Log;
+
+public class Tools {
+	public final static String meetings_directory = "/storage/extSdCard/MM/MEETINGS";
+	public static void show(String data)
+	{
+	    Log.e("stuff", data);
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
